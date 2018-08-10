ff61b0eb100e8635a0aae9a0290599cc5f9f0e10
unknown <victorneveslokao@gmail.com>
2018-08-10 20:34:07 -0300

    Exercicio 01

---
+++ Sequencia/01.py
@@ -0,0 +1,8 @@
+'''
+1. Fornecida a quantidade e o preço unitário de determinada peça,
+calcular e exibir o total a pagar, sabendo que haverá um desconto de 10%.
+'''
+
+preco=float(input("Digite o valor unitario: "))
+quantidade=int(input("Digite a quantidade de pecas: "))
+print(preco*quantidade*0.9)
