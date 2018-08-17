906393f29ac61d4166661d206abfa33734531b7f
unknown <38274108+neveSZ@users.noreply.github.com>
2018-08-17 20:11:27 -0300

    Exercicio 09

---
+++ IAL-002/1-Sequência/09.py
@@ -0,0 +1,14 @@
+'''
+9. Dados os tempos gastos em uma viagem com caminhada,
+ônibus e metro (em minutos) exibir o tempo total gasto
+em três unidades: em horas, em minutos e em segundos.
+'''
+
+caminhada=int(input("caminhada: "))
+onibus=int(input("onibus: "))
+metro=int(input("metro: "))
+print("Horas: ", (caminhada+onibus+metro)/60)
+print("Minutos: ", caminhada+onibus+metro)
+print("Segundos", (caminhada+onibus+metro)*60)
+
+
