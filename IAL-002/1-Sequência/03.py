2e302285fd657a502db99edf1795eadae3db6417
unknown <38274108+neveSZ@users.noreply.github.com>
2018-08-17 19:36:30 -0300

    Exercicio 3

---
+++ IAL-002/1-Sequência/03.py
@@ -0,0 +1,8 @@
+'''
+3. Fornecido um número inteiro n (n≥10), exibir
+o valor correspondente aos dois dígitos mais à
+direita de n, sem utilizar o operador de resto.
+'''
+
+n=int(input("Digite o valor de n: "))
+print(n-(n//100*100))
