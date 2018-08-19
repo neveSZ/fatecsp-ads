51009b7b915cef7346907db07d544c7eeee38af6
unknown <38274108+neveSZ@users.noreply.github.com>
2018-08-19 15:44:09 -0300

    Exercicio 06

---
+++ IAL-002/2-Seleção/06.py
@@ -0,0 +1,18 @@
+'''
+6. Fornecido os coeficientes de uma equação de segundo grau (com a≠0, ou seja, não é necessário verificar a existência da equação), exibir suas raízes.
+Obs. (1): Para calcular a raiz quadrada de Δ, utilizar o operador de potência (**).
+Obs. (2): Caso Δ seja negativo, imprimir ‘não existem raízes reais’
+'''
+
+a=float(input('a: '))
+b=float(input('b: '))
+c=float(input('c: '))
+delta=b*b-4*a*c
+if delta==0:
+    print((-b+delta**(1/2))/(2*a))
+else:
+    if delta>0:
+        print((-b+delta**(1/2))/(2*a))
+        print((-b-delta**(1/2))/(2*a))
+    else:
+        print('não existem raízes reais')
