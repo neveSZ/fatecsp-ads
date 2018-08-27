eaa995ff390c5a0249dd7a1a1bfd30781d6b5761
Victor Neves Silva <vnsilva2000@gmail.com>
2018-08-27 19:20:29 -0300

    Correcao

--- IAL-002/3-Laços de Repetição/02-lista1.py
+++ IAL-002/3-Laços de Repetição/02-lista1.py
@@ -2,5 +2,5 @@
 Dado um numero inteiro n(n > 1), exibir os numeros de 1 ate n.
 '''
 n = int(input('n: '))
-for n in range(1, n+1):
+for n in range(1, n):
     print(n)
--- IAL-002/3-Laços de Repetição/03-lista1.py
+++ IAL-002/3-Laços de Repetição/03-lista1.py
@@ -2,5 +2,5 @@
 Dado um numero inteiro n(n > 1), exibir os numeros de n ate 1.
 '''
 n = int(input('n: '))
-for n in range(n, 2):
+for n in range(n, 1):
     print(n)
