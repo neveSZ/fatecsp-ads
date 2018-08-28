47b04e2980af1448c2cd7f7afc2c0c789fad2e17
Victor Neves Silva <vnsilva2000@gmail.com>
2018-08-28 19:48:44 -0300

    Exercicio 03

---
+++ IAL-002/3-Laços de Repetição/Lista 02/03.py
@@ -0,0 +1,13 @@
+'''
+Fornecidas letras maiusculas, lidas uma por vez, ate ocorrer uma fora de ordem,
+exibir a quantidade de letras lidas, exceto a letra fora de ordem.
+Exemplo: 'A','C','K','M','M','T','B' -> 6
+'''
+atual = ''
+antiga = ''
+contador = -1
+while (atual >= antiga):
+    antiga = atual
+    atual = input('letra: ')
+    contador = contador+1
+print(contador)
