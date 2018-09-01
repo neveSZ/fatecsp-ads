2d43f82cb6d25bb48038e40302cbbd2667d1f66f
Victor Neves Silva <vnsilva2000@gmail.com>
2018-09-01 14:27:01 -0300

    Exercicio Programa 01 (Push apos dia 12/09)

---
+++ IAL-002/Exercícios Programa/EP01.py
@@ -0,0 +1,15 @@
+'''
++-----------------------------------------------------------------------------+
+| Instituição   :  Faculdade de Tecnologia de São Paulo                       |
+| Departamento  :  Tecnologia da Informação                                   |
+| Curso         :  Análise e Desenvolvimento de Sistemas                      |
+| Disciplina    :  IAL-002                                                    |
+| Turno         :  Vespertino                                                 |
+| Aluno         :  Victor Neves Silva                                         |
+| Matrícula     :  18203756                                                   |
++-----------------------------------------------------------------------------+
+| Programa      :  EP01.py - O Amanhã                                         |
+| Linguagem     :  Python 3                                                   |
+| Compilador    :  CPython (3.6.2)                                            |
++-----------------------------------------------------------------------------+
+'''
