2895758099bd3b419e824347a45b8de0e9fccd59
Victor Neves Silva <vnsilva2000@gmail.com>
2018-09-28 23:22:08 -0300

    Exercicio 12

---
+++ IAL-002/Listas/4 - Funções/12.py
@@ -0,0 +1,5 @@
+def area_circulo(r):
+    # Recebe: um valor real r (r>=0) representando o raio de um circulo.
+    # Retorna: um valor real com a area do circulo.
+    # Obs: adote π=3.1416.
+    return 3.1416*r*r
