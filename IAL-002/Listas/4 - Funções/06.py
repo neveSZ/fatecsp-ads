cea8db61bf4b19ec35ef1c10e8dd13111b98b911
Victor Neves Silva <vnsilva2000@gmail.com>
2018-09-28 22:52:45 -0300

    Exercicio 06

---
+++ IAL-002/Listas/4 - Funções/06.py
@@ -0,0 +1,8 @@
+def tamanho(n):
+    # Recebe: um inteiro n (n>=0).
+    # Retorna: um inteiro com a quantidade de algarismos de n.
+    algarismo = 1
+    while(n >= 10):
+        n = n//10
+        algarismo = algarismo+1
+    return algarismo
