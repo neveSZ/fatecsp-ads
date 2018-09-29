c8d01991d505b16f7d7d731c1f719fef841702d3
Victor Neves Silva <vnsilva2000@gmail.com>
2018-09-28 23:22:03 -0300

    Exercicio 11

---
+++ IAL-002/Listas/4 - Funções/11.py
@@ -0,0 +1,5 @@
+def letra(n):
+    # Recebe: um valor inteiro n(1<=n<=26).
+    # Retorna: um caractere com a letra maiuscula correspodente no alfabeto.
+    # Obs: usar as funcoes built-int chr() e ord().
+    return chr(64+n)
