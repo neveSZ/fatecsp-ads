966f52abac48fbe576761a8ad97ce13d1ca8b200
Victor Neves Silva <vnsilva2000@gmail.com>
2018-10-04 09:30:53 -0300

    Alterado para funcionar para numeros positivos e negativos

--- IAL-002/Listas/4-Funções/06.py
+++ IAL-002/Listas/4-Funções/06.py
@@ -2,7 +2,7 @@ def tamanho(n):
     # Recebe: um inteiro n (n>=0).
     # Retorna: um inteiro com a quantidade de algarismos de n.
     algarismo = 1
-    while(n >= 10):
+    while n >= 10 or n <= -10:
         n = n/10
         algarismo = algarismo+1
     return algarismo
