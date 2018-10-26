c61ad9a405f415f457462c228e396cc5e5339ab1
Victor Neves Silva <38274108+neveSZ@users.noreply.github.com>
2018-10-26 10:48:59 -0300

    Correcao do enunciado

--- IAL-002/Listas/5-Listas homogêneas/Lista 01/c.py
+++ IAL-002/Listas/5-Listas homogêneas/Lista 01/c.py
@@ -1,6 +1,6 @@
 def zera_ind_par(a, tam):
     # Recebe: vetor a, com tam elementos.
-    # Retorna: zera os elementos de a com indice par.
+    # Acao: zera os elementos de a com indice par.
     for i in range(tam):
         if i % 2 == 0:
             a[i] = 0
