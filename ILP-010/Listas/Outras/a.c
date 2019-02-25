84966c801956e0ac8fad574c4483768436a65de6
VIctor Neves Silva <38274108+neveSZ@users.noreply.github.com>
2019-02-25 11:03:14 -0300

    Exercicio A

---
+++ ILP-010/Listas/Outras/a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*a) Crie um programa que leia a base e a altura de um triangulo(valores reais) e
+exiba sua area com duas casas decimais.*/
+int main()
+{
+  float base, altura, area;
+  printf("Base: ");
+  scanf("%f", &base);
+  printf("Altura: ");
+  scanf("%f", &altura);
+  area = base*altura/2;
+  printf("Area = %.2f", area);
+  return 0;
+}
