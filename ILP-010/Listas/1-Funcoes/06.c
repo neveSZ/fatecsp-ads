4f10b686f5f582f550092900191ea4ab7ac5888e
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-16 14:18:55 -0300

    Exercicio 06

---
+++ ILP-010/Listas/1-Funcoes/06.c
@@ -0,0 +1,10 @@
+int maior(int a[], int tam){
+//Recebe: o endereco do vetor a e o seu tamanho tam (tam > 0).
+//Retorna: o valor do maior elemento de a.
+  int i, maior=a[tam-1];
+  for(i=0; i<tam-1; i++){
+    if(a[i]>maior)
+      maior = a[i];
+  }
+  return maior;
+}
