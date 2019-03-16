d562d4815b85575ab4668645ae84c6d0f8bc2974
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-16 14:19:18 -0300

    Exercicio 12

---
+++ ILP-010/Listas/1-Funcoes/12.c
@@ -0,0 +1,12 @@
+int copia_maiusculas(char dest[], char fonte[]){
+//Recebe: o endereco da string dest e o endereco da string fonte.
+//Retorna: a quantidade de letras maiusculas copiadas de fonte para dest.
+  int i, cont=0;
+  for(i=0; dest[i]!='\0'; i++){
+    if(fonte[i]>64 && fonte[i]<91){
+      dest[cont]=fonte[i];
+      cont++;
+    }
+  }
+  return cont;
+}
