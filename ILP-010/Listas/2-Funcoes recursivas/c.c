483b5b73bd2e645d2786fd65a632a5a3044ebd1b
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-24 09:48:05 -0300

    Exercicio C

---
+++ ILP-010/Listas/2-Funcoes recursivas/c.c
@@ -0,0 +1,7 @@
+int potencia(float b, int n) {
+  // Receber um numero real b (b > 0) e um numero inteiro n (n >= 0) e devolver
+  // o resultado de b elevado a n.
+  if (n == 0)
+    return 1;
+  return b * potencia(b, n - 1);
+}
