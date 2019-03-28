33e609f193c7d45d807dc2ad4f7bc0c22899af2e
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-28 08:33:53 -0300

    Exercicio Programa 01

--- ILP-010/Exercícios Programa/EP01.c
+++ ILP-010/Exercícios Programa/EP01.c
@@ -5,8 +5,8 @@
 | Curso         :  Análise e Desenvolvimento de Sistemas                      |
 | Disciplina    :  ILP-010                                                    |
 | Turno         :  Tarde                                                      |
-| Aluno         :  Victor Neves Silva                                         |
-| Matrícula     :  18203756                                                   |
+| Aluno         :  Victor Neves Silva/Yago Machado Bareia                     |
+| Matrícula     :  18203756/18110782                                          |
 +-----------------------------------------------------------------------------+
 | Programa      :  EP01.c  - Codificação e decodificação de mensagens         |
 | Linguagem     :  ANSI C                                                     |
@@ -17,24 +17,22 @@
 
 void codifica(char msg[], char cod[]);
 void decodifica(char cod[], char msg[]);
-int decimalValue(int codChar);
-int charValue(int codChar);
 
 int main(void) {
   int opcao;
-  char msg[50], cod[100];
+  char msg[51], cod[101];
   puts("Digite 1 para codificar ou 2 para decodificar");
-  scanf(" %d", &opcao);
+  scanf("%d", &opcao);
   getchar(); // Nao guarda o \n
   switch (opcao) {
   case 1:
-    puts("mensagem: ");
+    puts("Digite a mensagem: ");
     gets(msg);
     codifica(msg, cod);
     puts(cod);
     break;
   case 2:
-    puts("codigo: ");
+    puts("Digite o codigo: ");
     gets(cod);
     decodifica(cod, msg);
     puts(msg);
@@ -46,31 +44,35 @@ int main(void) {
   return 0;
 }
 
+int valorDecimal(int ascii) {
+  // Recebe: um valor que representa um caractere numerico na tabela ASCII.
+  // Retorna: um numero em base decimal.
+  // Ex: recebe 55(valor ASCII do char '7') e retorna 7.
+  return ascii - 48;
+}
+
+int valorAscii(int decimal) {
+  // Recebe: um numero em base decimal.
+  // Retorna: um valor que representa um caractere numerico na tabela ASCII.
+  // Ex: recebe 7 e retorna 55(valor ASCII do char '7').
+  return decimal + 48;
+}
+
 void codifica(char msg[], char cod[]) {
   int i, cont = 0;
   for (i = 0; msg[i] != '\0'; i++) {
-    cod[cont++] = charValue(msg[i] / 10); // Guarda algarismo de dezena
-    cod[cont++] = charValue(msg[i] % 10); // Guarda algarismo de unidade
+    cod[cont++] = valorAscii(msg[i] / 10); // Guarda algarismo de dezena
+    cod[cont++] = valorAscii(msg[i] % 10); // Guarda algarismo de unidade
   }
-  cod[cont] = '\0';
+  cod[cont] = '\0'; // Final da string
 }
 
 void decodifica(char cod[], char msg[]) {
   int i, cont = 0;
   for (i = 0; cod[i] != '\0'; i += 2) {
-    msg[cont] = decimalValue(cod[i]) * 10 +
-                decimalValue(cod[i + 1]); // Une os dois algarismos
+    msg[cont] = valorDecimal(cod[i]) * 10 +
+                valorDecimal(cod[i + 1]); // Une os dois algarismos
     cont++;
   }
   msg[cont] = '\0';
-}
-
-int decimalValue(int charValue) {
-  // Caractere para decimal. Ex: o char '7' eh 55 em decimal
-  return charValue - 48;
-}
-
-int charValue(int decimalValue) {
-  // Decimal para caractere. Ex: o decimal 55 eh o char '7'
-  return decimalValue + 48;
 }
