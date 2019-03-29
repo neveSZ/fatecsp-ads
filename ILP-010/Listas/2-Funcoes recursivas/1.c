13d6728a0f75e3e60e3b5dbd7e73ab0f9d8f9d72
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-29 09:24:11 -0300

    Exercicio 1(b)

--- ILP-010/Listas/2-Funcoes recursivas/1.c
+++ ILP-010/Listas/2-Funcoes recursivas/1.c
@@ -9,3 +9,17 @@ void imp(int n) {
 }
 // R: 14 chamadas recursivas
 // Saida: 2102302102
+
+// b) Simule para n = 5 e a[]={2, 7, -9, 5, 6}.
+int maximo(int a[], int n) {
+  if (n == 1)
+    return a[0];
+  else {
+    int aux = maximo(a, n - 1);
+    if (aux > a[n - 1])
+      return aux;
+    else
+      return a[n - 1];
+  }
+}
+// Retorno: 7
