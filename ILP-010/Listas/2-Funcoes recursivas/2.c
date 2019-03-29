f302ab23cdf71bbc7832dc2965fdef3cdf1c142c
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-29 09:28:04 -0300

    Exercicio 2(m)

--- ILP-010/Listas/2-Funcoes recursivas/2.c
+++ ILP-010/Listas/2-Funcoes recursivas/2.c
@@ -8,3 +8,15 @@ float potencia(float b, int n) {
     return 1;
   return b * potencia(b, n - 1);
 }
+
+// m) Receber um numero inteiro n e exibir 'sim', caso o digito mais a esquerda
+// seja par, ou 'nao', caso contrario. Use os operadores de divisao e resto.
+void esquerdaPar(int n) {
+  if (n < 10) {
+    if (n % 2 == 0)
+      printf("sim");
+    else
+      printf("nao");
+  } else
+    esquerdaPar(n / 10);
+}
