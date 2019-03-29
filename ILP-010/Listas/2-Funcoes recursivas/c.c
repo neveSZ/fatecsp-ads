2378877c19ce5097c6be446a157b0e9f35dd2e72
Victor Neves Silva <vnsilva2000@gmail.com>
2019-03-29 09:26:24 -0300

    Reorganizacao

--- ILP-010/Listas/2-Funcoes recursivas/c.c
+++
@@ -1,7 +0,0 @@
-float potencia(float b, int n) {
-  // Receber um numero real b (b > 0) e um numero inteiro n (n >= 0) e devolver
-  // o resultado de b elevado a n.
-  if (n == 0)
-    return 1;
-  return b * potencia(b, n - 1);
-}
