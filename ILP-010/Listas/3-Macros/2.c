d271f8eaab7f7a57c26ecd93d1c66316af64c1a1
VIctor Neves Silva <38274108+neveSZ@users.noreply.github.com>
2019-04-14 22:38:38 -0300

    Exercicio C

--- ILP-010/Listas/3-Macros/2.c
+++ ILP-010/Listas/3-Macros/2.c
@@ -5,3 +5,6 @@
 
 // b) Resulta 1 se x for impar ou 0 caso contrario;
 #define impar(x) x % 2 == 0 ? 0 : 1
+
+// c) Resulta 1 se ch for uma letra maiuscula ou 0 caso contrario;
+#define maiuscula(ch) ch > 64 && ch < 91 ? 1 : 0
