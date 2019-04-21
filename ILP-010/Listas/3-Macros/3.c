50433305d41408cf3090a490fe489b47d2e950cd
Victor Neves Silva <vnsilva2000@gmail.com>
2019-04-21 10:10:27 -0300

    Exercicio C

--- ILP-010/Listas/3-Macros/3.c
+++ ILP-010/Listas/3-Macros/3.c
@@ -1,2 +1,6 @@
 // 3. Defina as seguintes macros usando as proprias macros construidas no
 // exercicio anterior:
+
+// c) TO_LOWER(ch): Resulta a letra minuscula, caso ch seja uma letra maiuscula.
+#define maiuscula(ch) ch > 64 && ch < 91 ? 1 : 0
+#define TO_LOWER(ch) (maiuscula(ch)) ? ch + 32 : ch
